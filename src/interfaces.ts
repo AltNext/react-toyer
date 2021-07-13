40809eb3ef7eb66b876ad47ab1ce1bd980693e8d
Dmitry Dodzin <d.dodzin@gmail.com>
2021-07-13 16:32:28 +0300

    feat: make the rendering sort of work

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,13 +3,20 @@ interface ISized {
   height: number;
 }
 
+export interface IVideoItem {
+  index: number;
+  playing: boolean;
+  element: HTMLVideoElement;
+}
+
 export interface IToyerContext {
+  latestIndex: number;
   canvas: ISized;
+  videos: IVideoItem[];
 }
 
 export interface IToyerProps extends ISized {}
 
-export interface IToyerVideoProps extends Partial<ISized> {
-  playing?: boolean;
+export interface IToyerVideoProps extends Partial<ISized & Omit<IVideoItem, 'element'>> {
   src: string;
 }
--- src/toyer.tsx
+++ src/toyer.tsx
@@ -1,16 +1,81 @@
 import type { FC, VFC } from 'react';
-import { createContext, /* useContext,*/ useRef } from 'react';
+import { createContext, useContext, useEffect, useRef } from 'react';
 
-import type { IToyerContext, IToyerProps, IToyerVideoProps } from './interfaces';
+import type { IToyerContext, IToyerProps, IToyerVideoProps, IVideoItem } from './interfaces';
 
-export const ToyerContext = createContext<IToyerContext>({ canvas: { height: 0, width: 0 } });
+export const ToyerContext = createContext<IToyerContext>({
+  latestIndex: 0,
+  videos: [],
+  canvas: { height: 0, width: 0 },
+});
+
+const registerVideo = (context: IToyerContext, video: IVideoItem): (() => void) => {
+  video.element.addEventListener('loadedmetadata', () => {
+    context.videos[video.index] = video;
+  });
+
+  return () => {
+    if (context.videos[video.index]) {
+      delete context.videos[video.index];
+    }
+  };
+};
+
+const getIndex = (context: IToyerContext, index?: number): number => index ?? (context.latestIndex += 1);
 
 export const Toyer: FC<IToyerProps> = ({ children, height, width }) => {
-  const contextRef = useRef<IToyerContext>({ canvas: { height, width } });
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const contextRef = useRef<IToyerContext>({ canvas: { height, width }, latestIndex: 0, videos: [] });
+
+  useEffect(() => {
+    let running = true;
+    let ctx = canvasRef.current?.getContext('2d');
+
+    const tick = (): void => {
+      if (!running) {
+        return;
+      }
+
+      if (!ctx) {
+        ctx = canvasRef.current?.getContext('2d');
+
+        requestAnimationFrame(tick);
+
+        return;
+      }
+
+      ctx.clearRect(0, 0, contextRef.current.canvas.width, contextRef.current.canvas.height);
+
+      for (const video of Object.values(contextRef.current.videos)) {
+        if (video.playing && video.element.paused) {
+          video.element.play();
+        }
+
+        if (!video.playing && !video.element.paused) {
+          video.element.pause();
+          video.element.currentTime = 0;
+        }
+
+        if (video.playing) {
+          ctx.drawImage(video.element, 0, 0, video.element.width, video.element.height);
+        }
+      }
+
+      requestAnimationFrame(tick);
+    };
+
+    requestAnimationFrame(tick);
+
+    return () => {
+      running = false;
+    };
+  }, []);
+
+  contextRef.current.latestIndex = 0;
 
   return (
     <div style={{ height, width }}>
-      <canvas height={height} width={width} />
+      <canvas ref={canvasRef} height={height} width={width} />
       <ToyerContext.Provider value={contextRef.current}>{children}</ToyerContext.Provider>
     </div>
   );
@@ -18,9 +83,28 @@ export const Toyer: FC<IToyerProps> = ({ children, height, width }) => {
 
 Toyer.displayName = 'Toyer';
 
-export const ToyerVideo: VFC<IToyerVideoProps> = () =>
-  /* const context = useContext(ToyerContext); */
+export const ToyerVideo: VFC<IToyerVideoProps> = ({ index, src, playing, width, height }) => {
+  const context = useContext(ToyerContext);
 
-  null;
+  const currentIndex = getIndex(context, index);
+
+  useEffect(() => {
+    const video = document.createElement('video');
+
+    video.muted = true;
+    video.src = src;
+    video.width = width ?? context.canvas.width;
+    video.height = height ?? context.canvas.height;
+
+    const unregister = registerVideo(context, { index: currentIndex, playing: !!playing, element: video });
+
+    return () => {
+      unregister();
+      video.remove();
+    };
+  }, [context, src, currentIndex, width, height]); // Its intentinal to keep out playing from deps here
+
+  return null;
+};
 
 ToyerVideo.displayName = 'ToyerVideo';
